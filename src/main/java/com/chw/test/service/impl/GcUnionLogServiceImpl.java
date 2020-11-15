d3dda7c6754a6c1afc8fa0e9761d7742c39e0279
ChenWei <545764502@qq.com>
2020-11-15 11:32:31 +0800

    增加gc监控统计

--- src/main/java/com/chw/test/service/impl/GcUnionLogServiceImpl.java
+++ src/main/java/com/chw/test/service/impl/GcUnionLogServiceImpl.java
@@ -1,5 +1,6 @@
 package com.chw.test.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.chw.test.dto.JvmGcJson;
@@ -7,12 +8,16 @@ import com.chw.test.dto.Measurement;
 import com.chw.test.entity.GcUnionLog;
 import com.chw.test.mapper.GcUnionLogMapper;
 import com.chw.test.service.GcUnionLogService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
@@ -26,8 +31,11 @@ import java.util.List;
  * @author ChenWei
  * @since 2020-11-14
  */
+@Slf4j
 @Service
 public class GcUnionLogServiceImpl extends ServiceImpl<GcUnionLogMapper, GcUnionLog> implements GcUnionLogService {
+
+    private String token="Bearer7de818f4-db62-44fb-850b-3de3ba8eb6c1";
 
     @Resource
     private RestTemplate restTemplate;
@@ -37,9 +45,27 @@ public class GcUnionLogServiceImpl extends ServiceImpl<GcUnionLogMapper, GcUnion
     @Override
     public void saveGcLog() {
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization","Bearer7de818f4-db62-44fb-850b-3de3ba8eb6c1");
+        headers.set("Authorization",token);
         HttpEntity requestEntity = new HttpEntity(headers);
-        ResponseEntity<JvmGcJson> exchange = restTemplate.exchange(url, HttpMethod.GET, requestEntity, JvmGcJson.class);
+        ResponseEntity<JvmGcJson> exchange;
+        try {
+            exchange = restTemplate.exchange(url, HttpMethod.GET, requestEntity, JvmGcJson.class);
+        } catch (HttpClientErrorException e) {
+            MultiValueMap<String, String> params= new LinkedMultiValueMap<>();
+            params.add("username", "15387976997");
+            params.add("password", "976997");
+            params.add("scope","all");
+            params.add("grant_type","password");
+            params.add("platform","1");
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.set("Authorization","Basic c21hcnQ6MGQ1NTcwNzhiYTViNGYxYzk4NzU3ZjI2MjJmY2QyYTk=");
+            ResponseEntity<String> auth = restTemplate.exchange("http://jz.jtyedu.com/v1/smart/smart-school-auth/oauth/token",
+                    HttpMethod.POST, new HttpEntity<>(params, httpHeaders), String.class);
+            token=JSON.parseObject(auth.getBody()).getJSONObject("data").getString("access_token");
+            log.info("---获取token成功---token={}",token);
+            this.saveGcLog();
+            return;
+        }
         JvmGcJson body = exchange.getBody();
         List<Measurement> measurements = body.getMeasurements();
         if(CollectionUtils.isEmpty(measurements)){
--- src/main/java/com/chw/test/task/MarkMonitor.java
+++ src/main/java/com/chw/test/task/MarkMonitor.java
@@ -22,12 +22,12 @@ import java.time.LocalDateTime;
 @Component
 public class MarkMonitor {
 
-    private static final String url1 = "http://xn.jtyedu.com/proxy/v1/smart/yuejuan-single-business/druid/weburi-/api/markingTask/getPaper.json";
-    private static final String url2 = "http://xn.jtyedu.com/proxy/v1/smart/yuejuan-single-business/druid/weburi-/api/markingTask/submitScore.json";
-    private static final String url3 = "http://xn.jtyedu.com/proxy/v1/smart/yuejuan-union-business/druid/weburi-/api/markingTask/getPaper.json";
-    private static final String url4 = "http://xn.jtyedu.com/proxy/v1/smart/yuejuan-union-business/druid/weburi-/api/markingTask/submitScore.json";
-    private static final String url5 = "http://xn.jtyedu.com/proxy/v1/smart/yuejuan-single-business/druid/webapp.json";
-    private static final String url6 = "http://xn.jtyedu.com/proxy/v1/smart/yuejuan-union-business/druid/webapp.json";
+    private static final String url1 = "http://jz.jtyedu.com/v1/smart/yuejuan-single-business/druid/weburi-/api/markingTask/getPaper.json";
+    private static final String url2 = "http://jz.jtyedu.com/v1/smart/yuejuan-single-business/druid/weburi-/api/markingTask/submitScore.json";
+    private static final String url3 = "http://jz.jtyedu.com/v1/smart/yuejuan-union-business/druid/weburi-/api/teacher/markingTask/getPaper.json";
+    private static final String url4 = "http://jz.jtyedu.com/v1/smart/yuejuan-union-business/druid/weburi-/api/teacher/markingTask/submitScore.json";
+    private static final String url5 = "http://jz.jtyedu.com/v1/smart/yuejuan-single-business/druid/webapp.json";
+    private static final String url6 = "http://jz.jtyedu.com/v1/smart/yuejuan-union-business/druid/webapp.json";
 
     @Resource
     private RestTemplate restTemplate;
@@ -67,42 +67,46 @@ public class MarkMonitor {
         params.add("loginPassword", "123456");
         HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(params, headers);
         try {
-            ResponseEntity<String> response1 = restTemplate.exchange(url1, HttpMethod.POST, requestEntity, String.class);
-            DruidUrlMonitorResultJson druidUrlMonitorResultJson1 = JSON.parseObject(response1.getBody(), DruidUrlMonitorResultJson.class);
-            if(druidUrlMonitorResultJson1!=null && druidUrlMonitorResultJson1.getContent()!=null){
-                monitorSingleGetPaperService.save(MonitorSingleGetPaper.getBean(druidUrlMonitorResultJson1.getContent()));
-            }
-            ResponseEntity<String> response2 = restTemplate.exchange(url2, HttpMethod.POST, requestEntity, String.class);
-            DruidUrlMonitorResultJson druidUrlMonitorResultJson2 = JSON.parseObject(response2.getBody(), DruidUrlMonitorResultJson.class);
-            if(druidUrlMonitorResultJson2!=null && druidUrlMonitorResultJson2.getContent()!=null){
-                monitorSingleSubmitScoreService.save(MonitorSingleSubmitScore.getBean(druidUrlMonitorResultJson2.getContent()));
-            }
-            ResponseEntity<String> response3 = restTemplate.exchange(url3, HttpMethod.POST, requestEntity, String.class);
-            DruidUrlMonitorResultJson druidUrlMonitorResultJson3 = JSON.parseObject(response3.getBody(), DruidUrlMonitorResultJson.class);
-            if(druidUrlMonitorResultJson3!=null && druidUrlMonitorResultJson3.getContent()!=null){
-                monitorUnionGetPaperService.save(MonitorUnionGetPaper.getBean(druidUrlMonitorResultJson3.getContent()));
-            }
-            ResponseEntity<String> response4 = restTemplate.exchange(url4, HttpMethod.POST, requestEntity, String.class);
-            DruidUrlMonitorResultJson druidUrlMonitorResultJson4 = JSON.parseObject(response4.getBody(), DruidUrlMonitorResultJson.class);
-            if(druidUrlMonitorResultJson4!=null && druidUrlMonitorResultJson4.getContent()!=null){
-                monitorUnionSubmitScoreService.save(MonitorUnionSubmitScore.getBean(druidUrlMonitorResultJson4.getContent()));
-            }
-            ResponseEntity<String> response5 = restTemplate.exchange(url5, HttpMethod.POST, requestEntity, String.class);
-            DruidUrlMonitorAllJson druidUrlMonitorResultJson5 = JSON.parseObject(response5.getBody(), DruidUrlMonitorAllJson.class);
-            if(druidUrlMonitorResultJson5!=null && druidUrlMonitorResultJson5.getContent().get(0)!=null){
-                monitorSingleAllService.save(MonitorSingleAll.getBean(druidUrlMonitorResultJson5.getContent().get(0)));
-            }
-            ResponseEntity<String> response6 = restTemplate.exchange(url6, HttpMethod.POST, requestEntity, String.class);
-            DruidUrlMonitorAllJson druidUrlMonitorResultJson6 = JSON.parseObject(response6.getBody(), DruidUrlMonitorAllJson.class);
-            if(druidUrlMonitorResultJson6!=null && druidUrlMonitorResultJson6.getContent().get(0)!=null){
-                monitorUnionAllService.save(MonitorUnionAll.getBean(druidUrlMonitorResultJson6.getContent().get(0)));
-            }
+            this.getMarkingLog(requestEntity);
             gcUnionLogService.saveGcLog();
-            sysUserService.getRecord(2);
         } catch (RestClientException e) {
             log.info(e.toString());
         }
         log.info("----获取监控记录完成----"+ LocalDateTime.now());
+    }
+
+    private void getMarkingLog(HttpEntity<MultiValueMap<String, String>> requestEntity){
+        ResponseEntity<String> response1 = restTemplate.exchange(url1, HttpMethod.POST, requestEntity, String.class);
+        DruidUrlMonitorResultJson druidUrlMonitorResultJson1 = JSON.parseObject(response1.getBody(), DruidUrlMonitorResultJson.class);
+        if(druidUrlMonitorResultJson1!=null && druidUrlMonitorResultJson1.getContent()!=null){
+            monitorSingleGetPaperService.save(MonitorSingleGetPaper.getBean(druidUrlMonitorResultJson1.getContent()));
+        }
+        ResponseEntity<String> response2 = restTemplate.exchange(url2, HttpMethod.POST, requestEntity, String.class);
+        DruidUrlMonitorResultJson druidUrlMonitorResultJson2 = JSON.parseObject(response2.getBody(), DruidUrlMonitorResultJson.class);
+        if(druidUrlMonitorResultJson2!=null && druidUrlMonitorResultJson2.getContent()!=null){
+            monitorSingleSubmitScoreService.save(MonitorSingleSubmitScore.getBean(druidUrlMonitorResultJson2.getContent()));
+        }
+        ResponseEntity<String> response3 = restTemplate.exchange(url3, HttpMethod.POST, requestEntity, String.class);
+        DruidUrlMonitorResultJson druidUrlMonitorResultJson3 = JSON.parseObject(response3.getBody(), DruidUrlMonitorResultJson.class);
+        if(druidUrlMonitorResultJson3!=null && druidUrlMonitorResultJson3.getContent()!=null){
+            monitorUnionGetPaperService.save(MonitorUnionGetPaper.getBean(druidUrlMonitorResultJson3.getContent()));
+        }
+        ResponseEntity<String> response4 = restTemplate.exchange(url4, HttpMethod.POST, requestEntity, String.class);
+        DruidUrlMonitorResultJson druidUrlMonitorResultJson4 = JSON.parseObject(response4.getBody(), DruidUrlMonitorResultJson.class);
+        if(druidUrlMonitorResultJson4!=null && druidUrlMonitorResultJson4.getContent()!=null){
+            monitorUnionSubmitScoreService.save(MonitorUnionSubmitScore.getBean(druidUrlMonitorResultJson4.getContent()));
+        }
+        ResponseEntity<String> response5 = restTemplate.exchange(url5, HttpMethod.POST, requestEntity, String.class);
+        DruidUrlMonitorAllJson druidUrlMonitorResultJson5 = JSON.parseObject(response5.getBody(), DruidUrlMonitorAllJson.class);
+        if(druidUrlMonitorResultJson5!=null && druidUrlMonitorResultJson5.getContent().get(0)!=null){
+            monitorSingleAllService.save(MonitorSingleAll.getBean(druidUrlMonitorResultJson5.getContent().get(0)));
+        }
+        ResponseEntity<String> response6 = restTemplate.exchange(url6, HttpMethod.POST, requestEntity, String.class);
+        DruidUrlMonitorAllJson druidUrlMonitorResultJson6 = JSON.parseObject(response6.getBody(), DruidUrlMonitorAllJson.class);
+        if(druidUrlMonitorResultJson6!=null && druidUrlMonitorResultJson6.getContent().get(0)!=null){
+            monitorUnionAllService.save(MonitorUnionAll.getBean(druidUrlMonitorResultJson6.getContent().get(0)));
+        }
+        sysUserService.getRecord(2);
     }
 
     /*
